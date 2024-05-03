1cb300410e60fdb0046caedeac0bc2b554567b63
timur <timurzavalov@gmail.com>
2024-05-03 11:31:53 +0300

    fix: enabled cache and getting all data from db

--- main.py
+++ main.py
@@ -28,8 +28,6 @@ class UpdatingCompanies:
             ref_compass: QueryResult = client.query(
                 "SELECT * "
                 "FROM reference_compass "
-                "ORDER BY last_updated NULLS FIRST, original_file_name "
-                "LIMIT 10000"
             )
             # Чтобы проверить, есть ли данные. Так как переменная образуется, но внутри нее могут быть ошибки.
             print(ref_compass.result_rows[0])
@@ -44,7 +42,7 @@ class UpdatingCompanies:
                     "dadata_branch_address": '',
                     "dadata_branch_region": '',
                     "last_updated": start_time,
-                    "from_cache": False
+                    "from_cache": True
                  } for row in ref_compass.result_rows
             ]
         except Exception as ex_connect:
