f960af416aea7b5de180bb22059c5be6f48933d4
timur <agladsoft@gmail.com>
2025-07-17 16:47:35 +0300

    add: added README.md

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ class UpdatingCompanies:
             ref_compass: QueryResult = client.query(
                 "SELECT * "
                 "FROM reference_compass "
-                "WHERE dadata_geo_lat = dadata_geo_lon "
-                "ORDER BY last_updated DESC"
+                "ORDER BY last_updated NULLS FIRST, original_file_name "
+                "LIMIT 19000"
             )
             # Чтобы проверить, есть ли данные. Так как переменная образуется, но внутри нее могут быть ошибки.
             print(ref_compass.result_rows[0])
